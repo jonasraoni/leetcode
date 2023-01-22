6f9d7a713dca2d74048405248e820b3fc696eb6e
Jonas Raoni Soares da Silva <github@raoni.org>
2023-01-22 22:41:02 +0300

    Added runningSum

--- pivotIndex.js
+++ pivotIndex.js
@@ -4,10 +4,8 @@
  */
 var pivotIndex = function(nums) {
     nums.reduce((t, c, i) => nums[i] = t + c, 0);
-    for (let i = -1; ++i < nums.length;) {
-        if (~~nums[i - 1] === nums.at(-1) - nums[i]) {
+    for (let i = -1, l = nums.at(-1); ++i < nums.length;)
+        if (~~nums[i - 1] === l - nums[i])
             return i;
-        }
-    }
     return -1;
 };
---
+++ runningSum.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    for (let i = 0; ++i < nums.length; nums[i] += nums[i - 1]);
+    return nums;
+};
+
+var runningSum2 = function(nums) {
+    return nums.map(n => nums += n, nums = 0);
+};
